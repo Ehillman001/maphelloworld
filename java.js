26d2aba34b6330757ab3afb43c261899724dbb9f
Ehillman001 <72094664+Ehillman001@users.noreply.github.com>
2021-01-28 14:57:02 -0500

    Update java.js

--- java.js
+++ java.js
@@ -1,15 +1,15 @@
 // Initialize and add the map
 function initMap() {
   // The location of Uluru
-  const carleton = { lat: 45.38771680786572, lng: -75.69605238832233 };
+  const ottawa = { lat: 45.38771680786572, lng: -75.69605238832233 };
   // The map, centered at Uluru
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 4,
-    center: carleton,
+    center: ottawa,
   });
   // The marker, positioned at Uluru
   const marker = new google.maps.Marker({
-    position: carleton university,
+    position: ottawa,
     map: map,
   });
 }
