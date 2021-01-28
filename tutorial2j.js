9022c0b2401523eb2215990bfa761dcf042bdcc1
Ehillman001 <72094664+Ehillman001@users.noreply.github.com>
2021-01-28 14:35:24 -0500

    Update tutorial2j.js

--- tutorial2j.js
+++ tutorial2j.js
@@ -1,5 +1,5 @@
 function initMap() {
-  const map2 = new google.maps.Map(document.getElementById("map2"), {
+  const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 3,
     center: { lat: -28.024, lng: 140.887 },
   });
@@ -9,7 +9,7 @@ function initMap() {
   // Note: The code uses the JavaScript Array.prototype.map() method to
   // create an array of markers based on a given "locations" array.
   // The map() method here has nothing to do with the Google Maps API.
-  const markers = locations.map2((location, i) => {
+  const markers = locations.map((location, i) => {
     return new google.maps.Marker({
       position: location,
       label: labels[i % labels.length],
