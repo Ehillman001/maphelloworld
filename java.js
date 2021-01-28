e560c00350d8014d888c6910a095472553e48b6e
Ehillman001 <72094664+Ehillman001@users.noreply.github.com>
2021-01-28 14:18:28 -0500

    Update java.js

--- java.js
+++ java.js
@@ -1,12 +1,15 @@
-let map;
-
+// Initialize and add the map
 function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    const Carleton= { lat: 45.38776201738197, lng: -75.69603093064909 },
-    zoom: 8,
+  // The location of Uluru
+  const uluru = { lat: -25.344, lng: 131.036 };
+  // The map, centered at Uluru
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 4,
+    center: uluru,
   });
+  // The marker, positioned at Uluru
   const marker = new google.maps.Marker({
-    position: Carleton,
+    position: uluru,
     map: map,
   });
 }
