7be1038fbe947d98eee413956c78877bfa89cc1c
Ehillman001 <72094664+Ehillman001@users.noreply.github.com>
2021-01-28 13:59:22 -0500

    Update java.js

--- java.js
+++ java.js
@@ -2,11 +2,11 @@ let map;
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
-    const Carleton University = { lat: 45.38776201738197, lng: -75.69603093064909 },
+    const Carleton= { lat: 45.38776201738197, lng: -75.69603093064909 },
     zoom: 8,
   });
   const marker = new google.maps.Marker({
-    position: Carleton University,
+    position: Carleton,
     map: map,
   });
 }
